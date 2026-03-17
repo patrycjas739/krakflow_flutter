a47bb64e9e8d73757194938d4294f34e01b155bd
Patrycja Sierszyńska <patusias99@o2.pl>
2026-03-17 18:47:13 +0100

    lab 03 projekt

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Text('witaj'),
+    );
+  }
+}
